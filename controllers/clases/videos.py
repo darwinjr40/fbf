1c42b6aa88d236ba754e61798e00e54830e37267
Javi <javier193061@gmail.com>
2023-06-12 18:54:59 -0400

    add videos

--- controllers/clases/videos.py
+++ controllers/clases/videos.py
@@ -1,9 +1,10 @@
 xd = 'G:\materias\cursos\python\descargados\Violence-Alert-System\Violence-Detection\Testing-videos\V_19.mp4'
 xd1 = 'G:\materias\cursos\python\descargados\Violence-Alert-System\Violence-Detection\Testing-videos\carcel.mp4'
+
 class Video:
-    VIDEO1 = xd
-    VIDEO2 = xd
-    VIDEO3 = xd
+    VIDEO1 = "V_19.mp4"
+    VIDEO2 = "V_17.mp4"
+    VIDEO3 = "V_21.mp4"
     
     
     
